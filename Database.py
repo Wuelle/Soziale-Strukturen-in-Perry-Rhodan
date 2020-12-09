85d46a1b6fb9e7759d90d1286a2b5dbe569c4120
Wuelle <simon.wuelker@arcor.de>
2020-12-09 23:49:18 +0100

    using bs4 instead of the wikimedia api

--- Database.py
+++ Database.py
@@ -1,23 +1,35 @@
-from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData
+from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, Boolean
+from sqlalchemy_utils import database_exists, create_database
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("sqlite://social_interactions.db", echo = True)
-meta = MetaData()
+Base = declarative_base()
 
-# Contains general Objects that can be part of an interaction, eg. a Person or a Superintelligence
-nodes = Table(
-   "nodes", meta, 
-   Column("id", Integer, primary_key=True), 
-   Column("name", String), 
-)
+class Node(Base):
+	__tablename__ = 'nodes'
 
-# Contains the data about a single interaction between two nodes
-edges = Table(
-	"edges", meta,
-	Column("id", Integer, primary_key=True),
-	Column("node_1", Integer),
-	Column("node_2", Integer),
-	Column("weight", Integer)
-)
+	id = Column(Integer, primary_key=True)
+	name = Column(String)
+	artificial = Column(Boolean)
 
-meta.create_all(engine)
-conn = engine.connect()
+class Relation(Base):
+	__tablename__ = "relations"
+
+	id = Column(Integer, primary_key=True)
+	node_1 = Column(Integer)
+	node_2 = Column(Integer)
+	weight = Column(Integer)
+
+
+
+
+engine = create_engine("sqlite:///social_interactions.db")
+if not database_exists(engine.url):
+	create_database(engine.url)
+
+Base.metadata.create_all(engine)
+ 
+# Bind the sessionmaker to engine
+new_session = sessionmaker()
+new_session.configure(bind=engine)
+
--- get_data.py
+++ get_data.py
@@ -1,7 +1,14 @@
 import requests
+from Database import Node, Relation, new_session
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import quote
 import re
 
 def getLinksOnPage(titles, plnamespace="*", filter=lambda x:True):
+	items = []
+	characters = []
 	params = {
 		"action": "query",
 		"prop": "links",
@@ -11,35 +18,76 @@ def getLinksOnPage(titles, plnamespace="*", filter=lambda x:True):
 		"format": "json"
 	}
 
-	items = []
 	while True:
 		response = requests.get("https://www.perrypedia.de/mediawiki/api.php", params=params).json()
 		links = response["query"]["pages"]
 		for k, v in links.items():
-		    for l in v["links"]:
-		    	if filter(l["title"]):
-		        	items.append(l["title"])
+			for l in v["links"]:
+				if filter(l["title"]):
+					items.append(l["title"])
 
 		if "continue" in response:
 			params["plcontinue"] = response["continue"]["plcontinue"]
 		else:
 			return items
+
+def getCharactersFromPagelist(items):
+	for item in items:
+		tables = pd.read_html("https://www.perrypedia.de/wiki/"+quote(item))
+		for table in tables:
+			characters += table["Name"].to_list()
+		print(item, " done")
+	return characters
+
+def getMainCharacters(url):
+	"""
+	Gets a list of the main characters from a given link to a PR Edition
+	"""
 	
+	response = requests.get(url)
+	soup = BeautifulSoup(response.text, 'html.parser')
+	tables = soup.find_all('table')
+
+	characters = []
+	for table in tables:
+		for tr in table.findAll("tr"):
+			trs = tr.findAll("td")
+			if trs:
+				if trs[0].text == "Hauptpersonen:":
+					print("iside tre")
+					for each in trs:
+						links = each.find_all('a')
+						for link in links:
+							characters.append(link["href"])
+					break
+		else:
+			continue
+		break
+	else:
+		print("nothing found")
+		raise RuntimeError
+	return characters
 
 
-# re.match("^Personen [A-Z]$", "Personen A")
+def getRemoteTableColumn(url):
+	pr_characters_artificial = []
+	tables = pd.read_html(url)
+	for table in tables:
+		pr_characters_artificial += table["Name"].to_list()
+	return pr_characters_artificial
 
-# pr_books = getLinksOnPage("Perry Rhodan-Heftromane", plnamespace="100")
-# pr_characters_natural = getLinksOnPage("Personen", plnamespace="0", filter=lambda x: re.match("^Personen [A-Z]$", x))
-# pr_characters_artificial = getLinksOnPage("Künstliche Individuen")
 
+# pages = getLinksOnPage("Personen", plnamespace="0", filter=lambda x: re.match("^Personen [A-Z]$", x))
+# pr_characters_natural = getCharactersFromPagelist(pages)
+# pr_characters_artificial = getRemoteTableColumn("https://www.perrypedia.de/wiki/K%C3%BCnstliche_Individuen")
 
-# print(pr_characters_artificial)
-response = requests.get("https://www.perrypedia.de/mediawiki/api.php", params={
-	"action":"parse",
-	"prop":"sections",
-	"page":"Künstliche Individuen",
-	"format":"json"
-}).json()["parse"]["sections"]
-for element in response:
-	print(element["line"], element["index"])
+# session = new_session()
+# for character in pr_characters_artificial:
+# 	session.add(Node(name=character, artificial=True))
+# for character in pr_characters_natural:
+# 	session.add(Node(name=character, artificial=False))
+# session.commit()
+
+pr_books = getLinksOnPage("Perry Rhodan-Heftromane", plnamespace="100")
+# tables = pd.read_html("https://www.perrypedia.de/wiki/"+pr_books[0])
+print(getMainCharacters("https://www.perrypedia.de/wiki/"+pr_books[0]))
