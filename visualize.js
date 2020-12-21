0721ef605bce309f94b1e5535bb5572a3a172860
Wuelle <simon.wuelker@arcor.de>
2020-12-21 16:55:36 +0100

    structure toolbar using collapsibles

--- visualize.js
+++ visualize.js
@@ -115,6 +115,21 @@ $(document).ready(() => {
 			}
 		}
 	});
+
+	// Add Collapsible text to the sidebar
+	var coll = document.getElementsByClassName("toggle_collapsible");
+
+	for (var i = 0; i < coll.length; i++) {
+	  coll[i].addEventListener("click", function(){
+	    this.classList.toggle("active");
+	    var content = this.nextElementSibling;
+	    if (content.style.display === "block") {
+	      content.style.display = "none";
+	    } else {
+	      content.style.display = "block";
+	    }
+	  });
+	}
 });
 
 function downloadSVG(){
