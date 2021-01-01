9c9aa3c3ba9e75ff22cd2d15f13e66d3acbee4e2
Wuelle <simon.wuelker@arcor.de>
2021-01-01 09:05:56 +0100

    got the basic select2 template working with ajax

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from download.Database import Node, Relation, Link, Zyklus, new_session
 from flask_sqlalchemy_session import flask_scoped_session
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, jsonify, request
+import config
+from math import ceil
 
 app = Flask(__name__)
 session = flask_scoped_session(new_session, app)
@@ -8,6 +10,20 @@ session = flask_scoped_session(new_session, app)
 @app.route("/")
 def home():
 	return "Hello World!"
+
+@app.route("/search_characters", methods=["GET"])
+def search():
+	pages = []
+	query = request.args["query"]
+	
+	num_characters = session.query(Node).filter(Node.name.like(f"%{query}%")).count()
+	min_index = int(request.args["page"]) * config.SELECT2_PAGESIZE
+	max_index = min(min_index + config.SELECT2_PAGESIZE, num_characters)
+
+	characters = session.query(Node).filter(Node.name.like(f"%{query}%")).all()[min_index:max_index]
+	results = [{"id":char.id, "text":char.name} for char in characters]
+
+	return jsonify(results=results, pagination={"more":num_characters > config.SELECT2_PAGESIZE})
 
 @app.route("/visualization")
 def visualization():
---
+++ config.py
@@ -0,0 +1,1 @@
+SELECT2_PAGESIZE = 10 # Number of elements to be displayed as one page
